51ea532cb19f90226355086432c49e0708e93750
jamie <jmezhang@gmail.com>
2023-10-17 15:31:20 -0500

    restricting miniboard based on previous box selection

--- src/components/largeboard.jsx
+++ src/components/largeboard.jsx
@@ -5,10 +5,12 @@ function Largeboard() {
   let largeDimension = 3;
   let initialState = Array(largeDimension)
     .fill(0)
-    .map((e) => Array(largeDimension).fill('💀'));
+    .map((e) => Array(largeDimension).fill(null));
 
   const [lbState, setLbState] = useState(initialState);
   const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [currentBoard, setCurrentBoard] = useState(null);
+  const [focusBoard, setFocusBoard] = useState(null);
 
   function handleMiniWin(row, col) {
     lbState[row][col] = currentPlayer;
@@ -19,19 +21,55 @@ function Largeboard() {
   for (let i = 0; i < largeDimension; i++) {
     mbLayout[i] = [];
     for (let j = 0; j < largeDimension; j++) {
-      mbLayout[i].push(
-        <Miniboard
-          key={`mb${i}${j}`}
-          currentPlayer={currentPlayer}
-          setCurrentPlayer={setCurrentPlayer}
-          className="miniboard"
-          miniboardID={i + j}
-          marker={lbState[i][j]}
-          handleWin={() => handleMiniWin(row, col)}
-        />
-      );
+      if (
+        !focusBoard ||
+        (focusBoard && focusBoard[0] === i && focusBoard[1] === j)
+      ) {
+        mbLayout[i].push(
+          <Miniboard
+            style={{ backgroundColor: 'red' }}
+            key={`mb${i}${j}`}
+            currentPlayer={currentPlayer}
+            setCurrentPlayer={setCurrentPlayer}
+            currentBoard={currentBoard}
+            setCurrentBoard={setCurrentBoard}
+            focused={true}
+            className="miniboard"
+            miniboardID={[i, j]}
+            marker={lbState[i][j]}
+            handleWin={() => handleMiniWin(row, col)}
+          />
+        );
+      } else {
+        mbLayout[i].push(
+          <Miniboard
+            style={{ backgroundColor: 'blue' }}
+            key={`mb${i}${j}`}
+            className="miniboard"
+            miniboardID={[i, j]}
+            marker={lbState[i][j]}
+            focused={false}
+          />
+        );
+      }
     }
   }
+
+  useEffect(() => {
+    if (currentBoard) {
+      console.log('current board', currentBoard);
+      console.log(
+        'state of current board',
+        lbState[currentBoard[0]][currentBoard[1]]
+      );
+      console.log('focused board', focusBoard);
+      if (lbState[currentBoard[0]][currentBoard[1]]) {
+        setFocusBoard(null);
+      } else {
+        setFocusBoard([currentBoard[0], currentBoard[1]]);
+      }
+    }
+  }, [currentBoard]);
 
   return <div className="largeBoard">{mbLayout}</div>;
 }
--- src/components/miniboard.jsx
+++ src/components/miniboard.jsx
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Box from './box';
 
-function Miniboard({ currentPlayer, setCurrentPlayer, marker, handleWin }) {
+function Miniboard({
+  currentPlayer,
+  setCurrentPlayer,
+  setCurrentBoard,
+  style,
+  focused,
+  marker,
+  handleWin,
+}) {
   let miniDimension = 3;
   let initialState = Array(miniDimension)
     .fill(0)
-    .map((e) => Array(miniDimension).fill(''));
+    .map((e) => Array(miniDimension).fill(null));
 
   const [mbState, setMbState] = useState(initialState);
 
   function handleClick(row, col) {
     mbState[row][col] = currentPlayer;
     setMbState([...mbState]);
-    console.log(currentPlayer);
+    console.log('current player', currentPlayer);
+    setCurrentBoard([row, col]);
     setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
   }
 
@@ -20,19 +29,34 @@ function Miniboard({ currentPlayer, setCurrentPlayer, marker, handleWin }) {
   for (let i = 0; i < miniDimension; i++) {
     boxLayout[i] = [];
     for (let j = 0; j < miniDimension; j++) {
-      boxLayout[i].push(
-        <Box
-          key={`box${i}${j}`}
-          className="box"
-          boxID={i + j}
-          marker={mbState[i][j]}
-          handleClick={() => handleClick(i, j)}
-        />
-      );
+      if (focused) {
+        boxLayout[i].push(
+          <Box
+            key={`box${i}${j}`}
+            className="box"
+            boxID={[i, j]}
+            marker={mbState[i][j]}
+            handleClick={() => handleClick(i, j)}
+          />
+        );
+      } else {
+        boxLayout[i].push(
+          <Box
+            key={`box${i}${j}`}
+            className="box"
+            boxID={[i, j]}
+            marker={mbState[i][j]}
+          />
+        );
+      }
     }
   }
 
-  return <div className="miniBoard">{boxLayout}</div>;
+  return (
+    <div className="miniBoard" style={style}>
+      {boxLayout}
+    </div>
+  );
 }
 
 export default Miniboard;
