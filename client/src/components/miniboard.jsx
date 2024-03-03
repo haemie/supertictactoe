56b0af4c7eaa1cdb17722ee91da414a506ba4ebf
haemie <jmezhang@gmail.com>
2024-03-03 02:02:05 -0600

    reduced excessive rerenders with useMemo, also fixed player not changing

--- client/src/components/miniboard.jsx
+++ client/src/components/miniboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 import Box from './box';
 import { checkWin } from '../utility/gameLogic';
 import PropTypes from 'prop-types';
@@ -16,61 +16,62 @@ function Miniboard({
   // console.log(miniState);
   // when a box is clicked, assign the current player to the miniboard state, which updates the box marker, and update current player
 
-  function handleClick(row, col) {
-    if (!miniState[row][col]) {
-      let newMiniState = [...miniState];
-      newMiniState[row][col] = currentPlayer;
-      setMiniState(newMiniState);
-      setCurrentBoard([row, col]);
-      setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
-    }
-  }
-
-  const dimension = miniState.length;
-
-  let boxLayout = Array(dimension);
-  for (let i = 0; i < dimension; i++) {
-    boxLayout[i] = [];
-    for (let j = 0; j < dimension; j++) {
-      if (focused) {
-        boxLayout[i].push(
-          <Box
-            key={`box${i}${j}`}
-            className="box"
-            boxID={[i, j]}
-            marker={miniState[i][j]}
-            handleClick={() => handleClick(i, j)}
-          />
-        );
-      } else {
-        boxLayout[i].push(
-          <Box
-            key={`box${i}${j}`}
-            className="box"
-            boxID={[i, j]}
-            marker={miniState[i][j]}
-          />
-        );
+  const handleClick = useCallback(
+    (row, col) => {
+      if (!miniState[row][col]) {
+        let newMiniState = [...miniState];
+        newMiniState[row][col] = currentPlayer;
+        setMiniState(newMiniState);
+        setCurrentBoard([row, col]);
+        console.log(currentPlayer);
+        setCurrentPlayer((prevPlayer) => (prevPlayer === 'X' ? 'O' : 'X'));
       }
-    }
-  }
+    },
+    [currentPlayer, setCurrentPlayer, miniState, setMiniState, setCurrentBoard]
+  );
+
+  const boxLayoutMemo = useMemo(
+    () =>
+      miniState.map((row, i) =>
+        row.map((e, j) => {
+          // console.log('refresh');
+          return focused ? (
+            <Box
+              key={`box${i}${j}`}
+              className="box"
+              boxID={[i, j]}
+              marker={miniState[i][j]}
+              handleClick={() => handleClick(i, j)}
+            />
+          ) : (
+            <Box
+              key={`box${i}${j}`}
+              className="box"
+              boxID={[i, j]}
+              marker={miniState[i][j]}
+            />
+          );
+        })
+      ),
+    [miniState, focused, handleClick]
+  );
 
   useEffect(() => {
     // check for any win states
     if (checkWin(miniState)) {
       handleWin();
     }
-  }, [miniState]);
+  }, [miniState, handleWin]);
 
   return (
     <div
       className={focused ? 'focusedBoard' : 'miniBoard'}
       style={{
         ...style,
-        gridTemplate: `repeat(${dimension}, 1fr) / repeat(${dimension}, 1fr)`,
+        gridTemplate: `repeat(${miniState.length}, 1fr) / repeat(${miniState.length}, 1fr)`,
       }}
     >
-      {boxLayout}
+      {boxLayoutMemo}
     </div>
   );
 }
